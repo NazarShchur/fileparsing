f5204f750a6b4af6948bbe151973ba4b38a4dee4
NazarShchur <51239588+NazarShchur@users.noreply.github.com>
2020-10-13 16:42:13 +0300

    Initial commit

---
+++ src/test/java/com/nazar/fileparsing/FileparsingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nazar.fileparsing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileparsingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
