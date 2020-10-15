2b75f42ea8f22987f71c51f7b8dad389711ec026
NazarShchur <51239588+NazarShchur@users.noreply.github.com>
2020-10-15 16:42:09 +0300

    get parameters in post request

--- src/main/java/com/nazar/fileparsing/controller/TestController.java
+++ src/main/java/com/nazar/fileparsing/controller/TestController.java
@@ -5,12 +5,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
     @PostMapping("/")
-    public ResponseEntity testPost(){
+    public ResponseEntity parseAvroAndWriteToBigQuery(@RequestParam String bucketName, @RequestParam String objectName){
+        System.out.println(bucketName + "" + objectName);
         return new ResponseEntity(HttpStatus.CREATED);
     }
     @GetMapping("/")
