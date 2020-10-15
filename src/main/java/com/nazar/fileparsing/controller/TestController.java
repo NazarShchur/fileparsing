550107ad2694e3c8b1e2d9f821955e7a7b7351cc
NazarShchur <51239588+NazarShchur@users.noreply.github.com>
2020-10-15 15:53:04 +0300

    test

--- src/main/java/com/nazar/fileparsing/controller/TestController.java
+++ src/main/java/com/nazar/fileparsing/controller/TestController.java
@@ -2,6 +2,7 @@ package com.nazar.fileparsing.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
     @PostMapping("/")
-    public ResponseEntity test(){
+    public ResponseEntity testPost(){
         return new ResponseEntity(HttpStatus.CREATED);
+    }
+    @GetMapping("/")
+    public String testGet(){
+        return "HI";
     }
 }
