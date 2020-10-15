d2f3f3f059f2ac12c8c1a665044ac497bde5afab
NazarShchur <51239588+NazarShchur@users.noreply.github.com>
2020-10-15 15:46:27 +0300

    add entity

--- src/main/java/com/nazar/fileparsing/controller/TestController.java
+++ src/main/java/com/nazar/fileparsing/controller/TestController.java
@@ -1,12 +1,15 @@
 package com.nazar.fileparsing.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
-    @RequestMapping("/")
-    public String test(){
-        return "HEHE";
+    @PostMapping("/")
+    public ResponseEntity test(){
+        return new ResponseEntity(HttpStatus.FORBIDDEN);
     }
 }
