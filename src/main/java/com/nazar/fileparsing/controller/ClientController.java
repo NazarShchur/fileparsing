06174714471e5c049240643ba8b3d5008ff2b8d8
NazarShchur <51239588+NazarShchur@users.noreply.github.com>
2020-10-20 13:25:52 +0300

    refactor

--- src/main/java/com/nazar/fileparsing/controller/ClientController.java
+++ src/main/java/com/nazar/fileparsing/controller/ClientController.java
@@ -1,11 +1,11 @@
 package com.nazar.fileparsing.controller;
 
 import com.nazar.fileparsing.entity.Client;
+import com.nazar.fileparsing.service.AvroService;
 import com.nazar.fileparsing.service.ClientService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,12 +17,13 @@ import java.util.List;
 @AllArgsConstructor
 public class ClientController {
     private final ClientService clientService;
+    private final AvroService avroService;
 
     @PostMapping("/parse")
     public ResponseEntity parseAvroAndWriteToBigQuery(@RequestParam String bucketName, @RequestParam String objectName) throws IOException {
-        List<Client> clientList = clientService.parseAvro(bucketName, objectName);
+        List<Client> clientList = avroService.parseAvroClients(bucketName, objectName);
         clientService.save(clientList);
-        clientService.saveOptionals(clientList);
+        clientService.saveOptionalFields(clientList);
         return new ResponseEntity(HttpStatus.CREATED);
     }
 }
---
+++ src/main/java/com/nazar/fileparsing/exception/FileProblemException.java
@@ -0,0 +1,12 @@
+package com.nazar.fileparsing.exception;
+
+public class FileProblemException extends RuntimeException {
+    public FileProblemException() {
+        super();
+    }
+
+    public FileProblemException(String message) {
+        super(message);
+    }
+
+}
