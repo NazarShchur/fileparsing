a0f11a6ae0d373064036d4d7377da6052db3569e
NazarShchur <51239588+NazarShchur@users.noreply.github.com>
2021-03-14 19:33:47 +0200

    project change

--- src/main/java/com/nazar/fileparsing/controller/ClientController.java
+++ src/main/java/com/nazar/fileparsing/controller/ClientController.java
@@ -5,10 +5,12 @@ import com.nazar.fileparsing.entity.User;
 import com.nazar.fileparsing.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @AllArgsConstructor
+@RequestMapping("/")
 public class ClientController {
     private final UserRepository repository;
 
